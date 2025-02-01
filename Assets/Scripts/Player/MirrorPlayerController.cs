09d48173c2d8d120ad3cba6a70b80170fdd9c803
Oscar <oscar.boutani@gmail.com>
2025-02-01 17:01:59 +0100

    Kinda working player animation!??!

--- Assets/Scripts/Player/MirrorPlayerController.cs
+++ Assets/Scripts/Player/MirrorPlayerController.cs
@@ -4,6 +4,10 @@ using Mirror;
 public class MirrorPlayerController : NetworkBehaviour
 {
     public float moveSpeed = 5f;
+    public Animator animator;
+
+    private float lastMoveX = 0f;
+    private float lastMoveY = 0f;
 
     void Update()
     {
@@ -15,6 +19,36 @@ public class MirrorPlayerController : NetworkBehaviour
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
+        float speed = new Vector2(moveX, moveY).sqrMagnitude;
+
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetFloat("Speed", speed);
+
+        if (speed > 0.01f)
+        {
+            if (Mathf.Abs(moveX) > Mathf.Abs(moveY))
+            {
+                // Horizontal movement is stronger
+                lastMoveX = (moveX > 0) ? 1 : -1;
+                lastMoveY = 0;
+            }
+            else
+            {
+                // Vertical movement is stronger (or equal)
+                lastMoveX = 0;
+                lastMoveY = (moveY > 0) ? 1 : -1;
+            }
+        }
+
+        // Use the snapped directions if not moving
+        float displayX = (speed > 0.01f) ? lastMoveX : lastMoveX;
+        float displayY = (speed > 0.01f) ? lastMoveY : lastMoveY;
+
+        animator.SetFloat("Horizontal", displayX);
+        animator.SetFloat("Vertical", displayY);
+        animator.SetFloat("Speed", speed);
+
 
         // Send movement to the server
         CmdMove(moveX, moveY);
