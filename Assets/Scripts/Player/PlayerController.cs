8bcd52e83f8670ecd38bef06dcbd86606fcf3ec3
Oscar <oscar.boutani@gmail.com>
2025-02-02 18:15:18 +0100

    New Player Controller, Pixel Icon Pack

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,17 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerController : NetworkBehaviour
 {
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Vector2 _movement;
+
+    private Rigidbody2D _rb;
+    private Animator _animator;
+
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         if (!IsOwner) return; 
 
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
 
-        Vector3 movement = new Vector3(moveX, moveY, 0) * 5f * Time.deltaTime;
-        transform.position += movement;
+        _rb.linearVelocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
+
+        if (_movement != Vector2.zero)
+        {
+            _animator.SetFloat(_lastHorizontal, _movement.x);
+            _animator.SetFloat(_lastVertical, _movement.y);
+        }
 
         UpdatePositionServerRpc(transform.position);
     }
